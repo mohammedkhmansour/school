c0da01e6b23fb4e26f0a3477196c904d230c6beb
mohammed khader abu mansour <mohammedmansour0387@gmail.com>
2022-12-26 13:59:10 +0200

    Settings Teacher Dashboard

--- routes/teacher.php
+++ routes/teacher.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Teacher;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +24,11 @@ Route::group(
 
     //==============================dashboard============================
     Route::get('/teacher/dashboard', function () {
-        return view('pages.Teachers.dashboard.dashboard');
+        $ids = Teacher::findorFail(auth()->user()->id)->Sections()->pluck('section_id');
+        $data['count_sections']= $ids->count();
+        $data['count_students']= \App\Models\Student::whereIn('section_id',$ids)->count();
+
+        return view('pages.Teachers.dashboard.dashboard',$data);
     });
 
 });
