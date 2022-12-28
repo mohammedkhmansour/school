af7071d3b5502052f86657ed045b739fd6af769b
mohammed khader abu mansour <mohammedmansour0387@gmail.com>
2022-12-28 18:46:41 +0200

    Next Question With Livewire In Student Dashboard التالي في الاختبار وتحزبن الدرجة

--- resources/views/livewire/show-question.blade.php
+++ resources/views/livewire/show-question.blade.php
@@ -7,8 +7,7 @@
                 @foreach(preg_split('/(-)/', $data[$counter]->answers) as $index=>$answer)
                     <div class="custom-control custom-radio">
                         <input type="radio" id="customRadio{{$index}}" name="customRadio" class="custom-control-input" inh>
-                        <label class="custom-control-label" for="customRadio{{$index}}"> {{$answer}}</label>
-                    </div>
+                        <label class="custom-control-label" for="customRadio{{$index}}" wire:click="nextQuestion({{$data[$counter]->id}}, {{$data[$counter]->score}}, '{{$answer}}', '{{$data[$counter]->right_answer}}')"> {{$answer}}</label>                    </div>
                 @endforeach
 
             </div>
